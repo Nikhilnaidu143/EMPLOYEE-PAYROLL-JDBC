f82eebba6299d362c75bfbb0ff73f778c608e7ae
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-23 07:10:26 +0530

    START:- Displayed Welcome Message.

---
+++ src/main/java/com/payroll/jdbc/Payroll_JDBC.java
@@ -0,0 +1,7 @@
+package com.payroll.jdbc;
+
+public class Payroll_JDBC {
+	public static void main(String[] args) {
+		System.out.println("------------WELCOME TO EMPLOYEE PAYROLL JDBC-------------");
+	}
+}
